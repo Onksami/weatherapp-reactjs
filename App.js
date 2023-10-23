2540f392101758c7d2b138c8ab0f4a900d847687
Sami Onk <112951587+Onksami@users.noreply.github.com>
2023-10-23 11:09:22 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+// import Counter from './Project/Counter';
+// import TodoList from './Project/TodoList';
+// import TodoList1 from './Project/TodoList1';
+import WeatherApp from './Component/WeatherApp/WeatherApp';
+
+
+function App() {
+  return (
+    <>
+
+    <WeatherApp />
+
+
+
+
+
+
+
+    {/* <TodoList/> */}
+  
+    {/* <Counter /> */}
+    </>
+  );
+}
+
+export default App;
